f74f220bf119ca7017a24740002aa75ff310c957
ELCOT <ELCOT@Lenovo>
2024-08-31 11:34:30 +0530

    student service

---
+++ src/main/java/com/tnsif/pm/studentservice/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.tnsif.pm.studentservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> 
+{
+	
+	
+
+}
